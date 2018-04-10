c45d65eb067885730f052c32082464c179710139
Koushic Kannan <koushic-kannan.raju@hpe.com>
2018-04-10 17:40:16 +0530

    Commit changes

---
+++ src/com/hpe/base/Attributes.java
@@ -0,0 +1,7 @@
+package com.hpe.base;
+
+public enum Attributes {
+
+	TEXT, VALUE, TITLE, MAX_LENGTH, NAME ,GENERAL
+	
+}
---
+++ src/com/hpe/base/SelectBy.java
@@ -0,0 +1,6 @@
+package com.hpe.base;
+
+public enum SelectBy {
+
+	VALUE,INDEX,OPTION,VISIBLE_TEXT
+}
